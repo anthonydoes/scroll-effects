6080cde8ebd0c2694a63a0b5142a67d021225c0d
anthonydoes <anthonypyannos@gmail.com>
2021-07-31 19:50:32 -0400

    first commit - importing local project

---
+++ scrolls.js
@@ -0,0 +1,35 @@
+$(document).on('scroll', function() {
+    var pixelsFromTop = $(document).scrollTop()
+  
+    if (pixelsFromTop > 50) {
+      $('header').addClass('hidden')
+    } else {
+      $('header').removeClass('hidden')
+    }
+  
+    if (pixelsFromTop < 600) {
+      $('body').css('background-color', '#174869')
+    } else if (pixelsFromTop < 1400) {
+      $('body').css('background-color', '#305066')
+    } else if (pixelsFromTop < 2200) {
+      $('body').css('background-color', '#728ab0')
+    } else if (pixelsFromTop < 3000) {
+      $('body').css('background-color', '#5e192e')
+    } else {
+      $('body').css('background-color', '#756b90')
+    }
+  
+  
+    var documentHeight = $(document).height()
+    var windowHeight = $(window).height()
+  
+    var difference = documentHeight - windowHeight
+  
+    var percentage = 100 * pixelsFromTop / difference
+  
+    console.log(percentage)
+      
+    $('.bar').css('width', percentage + '%')
+    
+  })
+  
